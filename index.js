109ba446dace712b245fd1eedda39e8cf9d3282a
Bex Akeakamai <bex.akeakamai@gmail.com>
2021-12-02 15:53:30 -0500

    initial html

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
 
+document.getElementById("new-deck").addEventListener("click", handleClick)
+/**
+ * Challenge
+ * 
+ * Background:
+ * The Deck of Cards API expects us to provide the deck id 
+ * of the deck we're playing with so it can remember which
+ * cards we've already drawn, how many are remaining in the
+ * deck, etc.
+ * 
+ * Task: save the deck_id from the returned data to a local
+ * variable so we can use it later
+ */
