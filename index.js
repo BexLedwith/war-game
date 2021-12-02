4b148199c60bde1e010233280ff2929fe7c61974
Bex Akeakamai <bex.akeakamai@gmail.com>
2021-12-02 16:15:59 -0500

    draw 2 cards and toggle draw btn visibility

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+let deckId;
+
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then(function (data) {
       console.log(data);
-      const deckID = data.deck_id;
-      return deckID;
-    });
+      deckId = data.deck_id;
+    })
+    .then((document.getElementById("draw-cards").style.display = "block"));
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick);
@@ -21,3 +23,22 @@ document.getElementById("new-deck").addEventListener("click", handleClick);
  * Task: save the deck_id from the returned data to a local
  * variable so we can use it later
  */
+
+/**
+ * Challenge
+ *
+ * Task: Using the saved deckId, draw 2 new cards from the deck
+ *
+ * Docs for original Deck of Cards API: https://deckofcardsapi.com/#draw-card
+ * BaseUrl you'll use: https://apis.scrimba.com/deckofcards/api/deck/
+ * (that will replace the base url of https://deckofcardsapi.com/api/deck/)
+ * that you'll see in the deck of cards API docs.
+ */
+
+function draw() {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+document.getElementById("draw-cards").addEventListener("click", draw);
