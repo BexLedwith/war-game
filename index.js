7f95ddee99093ee8911c2864b507793b75631def
Bex Akeakamai <bex.akeakamai@gmail.com>
2021-12-03 12:00:51 -0500

    refactor using indexOf

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ let cardsImg = [];
 const newdeckBtn = document.getElementById("new-deck");
 const drawBtn = document.getElementById("draw-cards");
 const cardsContainer = document.getElementById("cards");
+
+const cardValsArr = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "JACK",
+  "QUEEN",
+  "KING",
+  "ACE",
+];
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then(function (data) {
-      console.log(data);
       deckId = data.deck_id;
     })
     .then((document.getElementById("draw-cards").style.display = "block"));
@@ -54,15 +69,43 @@ function draw() {
           <image src=${cardsImg[1]} class="card">`;
 
       cardsImg = [];
+      return cardsArr;
+    })
+    .then(function (cardsArr) {
+      const card1 = cardsArr[0].value;
+      const card2 = cardsArr[1].value;
+      const card1Index = cardValsArr.indexOf(card1);
+      const card2Index = cardValsArr.indexOf(card2);
+      card1Index > card2Index
+        ? console.log("Computer Wins!")
+        : card1Index < card2Index
+        ? console.log("You Win!")
+        : console.log("It's a Tie!");
     });
 }
 
 drawBtn.addEventListener("click", draw);
-
 /**
  * Challenge:
  *
- * Display the images of the 2 cards you drew in the browser.
- * Probably best to use `innerHTML` to insert a couple <img> elements
- * on the page.
+ * Try to determine which of the 2 cards is the "winner" (has higher value)
+ * Aces are the card with the highest "score"
+ *
+ * In parts:
+ *
+ * 1. Create a function that takes 2 card objects as parameters,
+ * `card1` and `card2`. These card objects have a property called
+ * `value`, which can be any one of the following strings, in
+ * order of rising "score":
+ *
+ * "2", "3", "4", "5", "6", "7", "8", "9",
+ * "10", "JACK", "QUEEN", "KING", "ACE"
+ *
+ * I.e. "2" is the lowest score and "ACE" is the highest.
+ *
+ * The function should determine which of the 2 cards (`card1`
+ * or `card2`) has the higher score, or if they have the same score.
+ *
+ * Log which card wins (or "It's a tie!"
+ * if they're the same) to the console
  */
